ae76d4892e099869528b1dbe22a639e1447552d8
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-06 15:53:56 +0900

    13-2. if else 조건문 실습

---
+++ cscondition/CodeFile1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cscondition
+{
+    class CodeFile1
+    {
+        static void Main(string[] args)
+        {
+            //오전 오후 구분
+
+            Console.WriteLine(DateTime.Now.Year);
+            Console.WriteLine(DateTime.Now.Month);
+            Console.WriteLine(DateTime.Now.Day);
+            Console.WriteLine(DateTime.Now.Hour);
+            Console.WriteLine(DateTime.Now.Minute);
+            Console.WriteLine(DateTime.Now.Second);
+            
+            if(DateTime.Now.Hour < 12)
+            {
+                Console.WriteLine("오전입니다.");
+            }
+            else
+            {
+                Console.WriteLine("오후입니다.");
+            }
+        }
+    }
+}
