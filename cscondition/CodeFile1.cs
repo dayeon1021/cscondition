112ac43598135e1c0ba21606168ac0784861757f
dayeon1021 <s2018s20@e-mirim.hs.kr>
2020-05-06 15:57:33 +0900

    13-3. 중첩 조건문 코드 구현

--- cscondition/CodeFile1.cs
+++ cscondition/CodeFile1.cs
@@ -19,13 +19,17 @@ namespace cscondition
             Console.WriteLine(DateTime.Now.Minute);
             Console.WriteLine(DateTime.Now.Second);
             
-            if(DateTime.Now.Hour < 12)
+            if(DateTime.Now.Hour < 11)
             {
-                Console.WriteLine("오전입니다.");
+                Console.WriteLine("아침 먹을 시간입니다.");
+            }
+            else if(DateTime.Now.Hour < 15)
+            {
+                Console.WriteLine("점심 먹을 시간입니다.");
             }
             else
             {
-                Console.WriteLine("오후입니다.");
+                Console.WriteLine("저녁 먹을 시간입니다.");
             }
         }
     }
